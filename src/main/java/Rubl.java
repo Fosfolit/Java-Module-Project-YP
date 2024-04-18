1565c5aaa301db26ae78090fb281ff1aa118c126
Fosfolit <bedarev_egorka@mail.ru>
2024-04-18 23:44:29 +0800

    Исправление окончания v2

--- src/main/java/Rubl.java
+++ src/main/java/Rubl.java
@@ -1,12 +1,19 @@
 public class Rubl {
     public String rubel(final double coast) {
-        double ost = coast % 10;
+        double ost = coast % 100;
+        if ((11 <= ost) && (ost < 19)) {
+            return "рублей";
+        }
+        ost = ost % 10;
         if ((5 <= ost) && (ost <= 9)) {
             return "рублей";
         }
-        if ((1 <= ost) && (ost < 2)) {
-            return "рублю";
+        if ((2 <= ost) && (ost <= 4)) {
+            return "рубля";
         }
-        return "рубля";
+        if ((1 <= ost) && (ost < 2)) {
+            return "рубль";
+        }
+        return "рублей";
     }
 }
